87eb942dff941ebf475357579baff25fb6915448
R-U-Bharti <kumar.bharti@gtropy.com>
2024-06-03 17:46:22 +0530

    kthLargestElement

--- Arrays/M-4-KthLargestElement.cpp
+++ Arrays/M-4-KthLargestElement.cpp
@@ -9,6 +9,7 @@ sort in descending coz' need to find largest else ascending
 #include <algorithm>
 #include <vector>
 #include <unordered_map>
+#include<limits.h>
 using namespace std;
 
 void printArray(vector<int> arr)
@@ -17,7 +18,26 @@ void printArray(vector<int> arr)
         cout << nums << " ";
 }
 
-int kthLargestElement(vector<int> nums, int k)
+APPROACH 1
+int getMaxNumber(vector<int> &arr, vector<int> &max_array)
+{
+    int max_number = INT_MIN;
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+
+        auto check = find(max_array.begin(), max_array.end(), arr[i]);
+
+        if (check == max_array.end())
+        {
+            max_number = max(max_number, arr[i]);
+        }
+    }
+
+    return max_number;
+}
+
+int kthLargestElement(vector<int> arr, int k)
 {
 
     // 🚀 With Sorting
@@ -27,8 +47,11 @@ int kthLargestElement(vector<int> nums, int k)
     // return nums[k-1];
 
     // 🚀Without Sorting
-    vector<int> arr = nums;
+    vector<int> max_array;
+    int max_number = 0;
     unordered_map<int, int> freq;
+    int idx = k;
+    int res = arr[0];
 
     // store element freq.
     for (int el : arr)
@@ -39,38 +62,48 @@ int kthLargestElement(vector<int> nums, int k)
     for (auto const i : freq)
         cout << i.first << "-->" << i.second << endl;
 
-    // find greatest number
-    int max_number = 0;
-    for (int n : arr)
-        max_number = max(max_number, n);
-
-    int cap = 0;
-    int target = k;
     for (int i = 0; i < arr.size(); i++)
     {
-        if (max_number > arr[i] && cap <= arr[i])
+        cout << "\nmax array: ";
+        printArray(max_array);
+
+        cout << "\nIterate element:" << arr[i] << endl;
+
+        cout << "Checking max_number \n";
+        max_number = getMaxNumber(arr, max_array);
+
+        max_array.push_back(max_number);
+        cout << "Pushed max number: " << max_number << " and freq. is: " << freq[max_number] << endl;
+
+        cout << "Current idx: " << idx << " and now changed to " << idx - freq[max_number] << endl;
+        idx = idx - freq[max_number];
+
+        if (idx <= 0)
         {
-            cap = arr[i];
-        }
-        else
-        {
-            target -= freq[arr[i]];
-            max_number = arr[i];
+            res = max_number;
+            break;
         }
     }
 
-    if (target == 1 || target == 0)
-    {
-        return max_number;
-    }
-
-    return max_number;
+    return res;
 }
+
+// APPROACH 2
+// int kthLargestElement(vector<int>arr, int k){
+//     return -1;
+// }
 
 int main()
 {
-    vector<int> arr = {3, 2, 3, 1, 2, 4, 5, 5, 6};
-    int k = 2;
+    // vector<int> arr = {3, 2, 3, 1, 2, 4, 5, 5, 6};
+    // int k = 3;
 
-    cout << k << "::element of array is: " << kthLargestElement(arr, k);
+    // vector<int> arr = {3, 2, 3, 1, 2, 4, 5, 5, 6};
+    // int k = 9;
+
+    vector<int> arr = {-1, 2, 0};
+    int k = 3;
+
+    cout << k << "::element of array is: ";
+    cout << kthLargestElement(arr, k);
 }
